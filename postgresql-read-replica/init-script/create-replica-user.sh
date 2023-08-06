c9e257511d64153a867d5efe27891328fc781e8c
Mohamed Sambo <77445231+sambo2021@users.noreply.github.com>
2023-08-06 22:30:20 +0300

    feat: password of replicator user in create-replica-user.sh

--- postgresql-read-replica/init-script/create-replica-user.sh
+++ postgresql-read-replica/init-script/create-replica-user.sh
@@ -1,5 +1,5 @@
 #create replicator user
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'my_replicator_password';
+	CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator';
     SELECT * FROM pg_create_physical_replication_slot('replication_slot_slave1');
 EOSQL
