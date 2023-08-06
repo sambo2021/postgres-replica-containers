d4f4906914cf78460177363a3f94decff25affa2
Mohamed Sambo <77445231+sambo2021@users.noreply.github.com>
2023-08-06 22:32:52 +0300

    feat: initiate only master database

--- postgresql-read-replica/docker-init.sh
+++ postgresql-read-replica/docker-init.sh
@@ -1,8 +1,3 @@
-echo "Clearing data"
-rm -rf ../postgresql-rp/data/*
-rm -rf ../postgresql-rp/data-slave/*
-docker-compose down
-
 docker-compose up -d  postgres_master
 
 echo "Starting postgres_master node..."
@@ -12,12 +7,4 @@ echo "Prepare replica config..."
 docker exec -it postgres_master sh /etc/postgresql/init-script/init.sh
 echo "Restart master node"
 docker-compose restart postgres_master
-sleep 5
 
-echo "Starting slave node..."
-docker-compose up -d  postgres_slave
-sleep 20  # Waits for note start complete
-
-echo "Done"
-
-docker exec -it postgres_master sh /etc/postgresql/init-script/validate.sh
